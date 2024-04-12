8cccba27f0bf60978d3fcfd0a76adad306274401
Frederico Marques <132377575+fmarques-mwb@users.noreply.github.com>
2024-04-12 19:25:33 +0100

    WIP: cmake not working

---
+++ c++/algorithms/sorting/test/TestSorting.cpp
@@ -0,0 +1,12 @@
+﻿// TestSorting.cpp : Defines the entry point for the application.
+//
+
+#include "TestSorting.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
